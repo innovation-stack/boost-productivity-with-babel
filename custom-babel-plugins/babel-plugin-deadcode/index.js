7014e5eaa33f49026aea54fd7119d752ddc4b94a
innovation-stack <anup.vasudeva2009@gmail.com>
2019-02-26 19:29:46 +0530

    Add service exclusions

--- custom-babel-plugins/babel-plugin-deadcode/index.js
+++ custom-babel-plugins/babel-plugin-deadcode/index.js
@@ -1,5 +1,7 @@
 /* eslint no-console: 0 */
 
+const serviceToFileNameCache = {};
+
 function getOuterChainedMethod(path) {
   let nextSiblingPath;
 
@@ -13,13 +15,38 @@ function getOuterChainedMethod(path) {
   return path;
 }
 
-function removeFromInjections(path) {
+function getFileNameFromServiceName(serviceName = '') {
+  let fileName;
+  let totalChars = serviceName.length;
+  if (serviceToFileNameCache[serviceName]) {
+    fileName = serviceToFileNameCache[serviceName];
+  } else if (serviceName !== '') {
+    try {
+      while (--totalChars) {
+        if (serviceName[totalChars] === serviceName[totalChars].toUpperCase()) {
+          const firstToken = serviceName[0].toLowerCase() + serviceName.substring(1, totalChars);
+          let secondToken = serviceName.substring(totalChars);
+          secondToken = secondToken[0].toLowerCase() + secondToken.substring(1);
+          fileName = `${firstToken}.${secondToken}`;
+          serviceToFileNameCache[serviceName] = fileName;
+          break;
+        }
+      }
+    } catch (ex) {
+      console.log('exception caught for: ', serviceName);
+    }
+  }
+  return fileName;
+}
+
+function removeFromInjections(path, servicesToExclude) {
   const calleePath = path.parentPath;
+
   if (calleePath.node.arguments.length === 2) {
     const injectionArgsPath = calleePath.get('arguments')[1];
     const properties = injectionArgsPath.node.properties;
     const index = properties.findIndex(function (p) {
-      return p.key.name === 'PhotoService';
+      return servicesToExclude.indexOf(getFileNameFromServiceName(p.key.name)) !== -1;
     });
     if (index >= 0) {
       const propertiesPath = injectionArgsPath.get(`properties.${index}`);
@@ -28,10 +55,10 @@ function removeFromInjections(path) {
   }
 }
 
-function removeInstantiation(path) {
+function removeInstantiation(path, servicesToExclude) {
   const parentPath = path.parentPath;
   const args = parentPath.node.arguments;
-  if (args[0].name === 'PHOTO_SERVICE' && args[1].name === 'PhotoService') {
+  if (servicesToExclude.indexOf(getFileNameFromServiceName(args[1].name)) !== -1) {
     parentPath.remove();
   }
 }
@@ -47,24 +74,33 @@ module.exports = function () {
   return {
     visitor: {
       ImportDeclaration: function (path, options) {
-        if (path.node && path.node.source.value.includes('photo.service')) {
-          console.log('Options 123: ', options.opts);
+        const exclusions = options.opts || {};
+        exclusions.services = exclusions.services || [];
+        let importFileName = path.node.source.value;
+        importFileName = importFileName.substring(importFileName.lastIndexOf('/') + 1);
+        if (path.node && exclusions.services.indexOf(importFileName) !== -1) {
           path.remove();
         }
       },
-      MemberExpression: function (path) {
+      MemberExpression: function (path, options) {
+        const exclusions = options.opts || {};
+        exclusions.services = exclusions.services || [];
+
         if (path.node.object.name === 'injections' && path.node.property.name === 'set') {
-          removeFromInjections(path);
+          removeFromInjections(path, exclusions.services);
         } else if (path.node.object.name === 'app' && path.node.property.name === 'service') {
-          removeInstantiation(path);
-        } else if (path.node.object.name === 'PhotoService') {
+          removeInstantiation(path, exclusions.services);
+        } else if (exclusions.services.indexOf(getFileNameFromServiceName(path.node.object.name)) !== -1) {
           removeMethodCalls(path);
         }
       },
-      ObjectPattern: function (path) {
+      ObjectPattern: function (path, options) {
+        const exclusions = options.opts || {};
+        exclusions.services = exclusions.services || [];
+
         if (path.node) {
           const index = path.node.properties.findIndex(function (p) {
-            return p.key.name === 'PhotoService';
+            return exclusions.services.indexOf(getFileNameFromServiceName(p.key.name)) !== -1;
           });
           if (index >= 0) {
             path.get(`properties.${index}`).remove();
